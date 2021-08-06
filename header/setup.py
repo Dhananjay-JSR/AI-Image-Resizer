e84c3656881860e1354729afca9ecd2e8b8da1db
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2021-08-06 15:55:59 +0530

    Update setup.py

--- header/setup.py
+++ header/setup.py
@@ -2,5 +2,7 @@ import cv2
 from cv2 import dnn_superres
 
 def start():
+
 sr = dnn_superres.DnnSuperResImpl_create()
+
 print("Project Manage By Dhananjay , Incase of any issue Open Issue Request on Github Repo")
